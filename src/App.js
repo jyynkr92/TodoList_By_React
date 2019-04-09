20de305675b94df5661aa984f13db44057423785
aliceyoo <jyynkr92@gmail.com>
2019-04-09 18:01:58 +0900

    add 'delete task' function

--- src/App.js
+++ src/App.js
@@ -44,14 +44,14 @@ class App extends Component {
 
   toggleTask = (key) => {
     const {todos} = this.state;
-    const testTodo = todos.filter(todo => todo.todoId === key);
+    const selectedTodo = todos.filter(todo => todo.todoId === key);
 
     const index = todos.findIndex(todo => todo.todoId === key);
 
     const todoList = [...todos]; // copy array
     todoList[index] = {
-      ...testTodo[0],
-      completeFlag : !testTodo[0].completeFlag
+      ...selectedTodo[0],
+      completeFlag : !selectedTodo[0].completeFlag
     };
 
     this.setState({
@@ -59,15 +59,22 @@ class App extends Component {
     });
   }
 
+  deleteTask = (key) => {
+    const {todos} = this.state;
+    this.setState ({
+      todos : todos.filter(todo => todo.todoId !== key)
+    });
+  }
+
   render() {
     const {input, todos} = this.state;
-    const {addTodo, onChange, keyEvent, toggleTask} = this;
+    const {addTodo, onChange, keyEvent, toggleTask, deleteTask} = this;
 
     return (
       <div className="todo_main">
         <div><h1>To Do List</h1></div>
         <TodoHeader todos={todos} addTodo={addTodo} change={onChange} input={input} keyPress={keyEvent}></TodoHeader>
-        <TodoSection todos={todos} toggleTask={toggleTask}></TodoSection>
+        <TodoSection todos={todos} toggleTask={toggleTask} deleteTask={deleteTask}></TodoSection>
         <TodoFooter todos={todos}></TodoFooter>
       </div>
     );
--- src/TodoSection.js
+++ src/TodoSection.js
@@ -2,26 +2,27 @@ import React, { Component} from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
 
-const TodoSection = ({todos, toggleTask}) => {
+const TodoSection = ({todos, toggleTask, deleteTask}) => {
     return (
         <ul className="todo_section">
-            <TodoList todos={todos} toggleTask={toggleTask}></TodoList>
+            <TodoList todos={todos} toggleTask={toggleTask} deleteTask={deleteTask}></TodoList>
         </ul>
     )
 }
 
-const TodoList  = ({todos, toggleTask}) => {
+const TodoList  = ({todos, toggleTask, deleteTask}) => {
     const todoList = todos.map(todo => {
-        return <TodoItem completeFlag={todo.completeFlag} todoId={todo.todoId} content={todo.content} key={todo.todoId} toggleTask={toggleTask}></TodoItem>
+        return <TodoItem completeFlag={todo.completeFlag} todoId={todo.todoId} content={todo.content} key={todo.todoId} toggleTask={toggleTask} deleteTask={deleteTask}></TodoItem>
     })
 
     return todoList;
 }
 
-const TodoItem = ({completeFlag, todoId, content, toggleTask}) => {
+const TodoItem = ({completeFlag, todoId, content, toggleTask, deleteTask}) => {
     return(
-        <li className={[completeFlag === true? "todo_complete" : "todo_progress", "todoItem"].join(" ")} key={todoId} onClick={() => toggleTask(todoId)}>
-            <span className="todo_content">{content}</span>
+        <li className={[completeFlag === true? "todo_complete" : "todo_progress", "todoItem"].join(" ")} key={todoId}>
+            <span className="todo_content" onClick={() => toggleTask(todoId)}>{content}</span>
+            <span className="todo_delete" onClick={() => deleteTask(todoId)}>삭제</span> 
         </li>
     )
 }
