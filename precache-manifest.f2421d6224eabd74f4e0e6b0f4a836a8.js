1edac5df932ffde4244d27dab83b5bc819442879
aliceyoo <jyynkr92@gmail.com>
2019-04-10 22:30:34 +0900

    Updates

---
+++ precache-manifest.f2421d6224eabd74f4e0e6b0f4a836a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo_react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8ae4c28430c1dff67705",
+    "url": "/todo_react/static/js/runtime~main.8f972959.js"
+  },
+  {
+    "revision": "32b01b454f7fdc0b3145",
+    "url": "/todo_react/static/js/main.4cb2c44d.chunk.js"
+  },
+  {
+    "revision": "c6bd0d203e320d08a191",
+    "url": "/todo_react/static/js/2.58b98f68.chunk.js"
+  },
+  {
+    "revision": "32b01b454f7fdc0b3145",
+    "url": "/todo_react/static/css/main.78290453.chunk.css"
+  },
+  {
+    "revision": "5d9cfc2259d247b003f8aa3bc4606c0a",
+    "url": "/todo_react/index.html"
+  }
+];
